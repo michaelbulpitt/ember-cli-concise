1eaffe3bb6f9b88e492a5b768585af2bcf4dde99
Michael Bulpitt <michael@kashoo.co.uk>
2015-02-20 21:45:31 +0000

    add import statements

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-concise'
+  name: 'ember-cli-concise',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/concise/dist/css/concise.css');
+    app.import(app.bowerDirectory + '/concise/dist/js/concise.js');
+  }
 };
